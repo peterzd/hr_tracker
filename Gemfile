18c458c4a9f116f3224959183e7ae6294f5c7712
peterzd.work <zdsunshine0640@126.com>
2013-05-14 17:11:43 +0800

    init repo

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+gem 'pg', '~> 0.15.1'
+gem 'thin', '~> 1.5.1'
+gem 'haml-rails', '~> 0.4'
+gem 'devise', '~> 2.2.3'
+gem 'cancan', '~> 1.6.9'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'factory_girl_rails', '~> 4.2.0'
+  gem 'rspec-rails', '~> 2.13.0'
+  gem 'faker', '~> 1.1.2'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HrTracker::Application.config.secret_token = '9183415f2ddfd2a2ab0942e5747febac054e3ebab7bc476e83e9da3542bf21e522d24b8826f5b2bafafd12682f011f1e1e5ccdd9bead7c230e18816be5d61861'
