fb9a519263d161518328716af80205c2703766b4
peterzd.work <zdsunshine0640@126.com>
2013-06-04 17:56:57 +0800

    refactoring the shared examples. Extract the page behavior to page_behavior

--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -10,52 +10,40 @@ describe EmployeesController do
 
     let(:request) { get :index }
     let(:assigns_assertion) { assigns[:employees].count == Employee.current_employees.count }
-    it_should_behave_like "access by the admin", 'index', '@employees'
-    it_should_behave_like "access by the normal employee", "index", "@employees"
+    let(:page_behavior) { render_template 'index' }
+    it_should_behave_like "access by the admin", '@employees'
+    it_should_behave_like "access by the normal employee",'index', "@employees"
   end
 
   describe "GET show" do
-
     let(:request) { get :show, id: peter.id }
-    it_should_behave_like "access by the admin", 'show'
+    let(:page_behavior) { render_template 'show' }
+    it_should_behave_like "access by the admin"
     it_should_behave_like "access by the normal employee", 'show'
 
-    it "for other's profile" do
+    it "denied for accessing other's page" do
       get :show, id: allen.id
       response.should redirect_to root_path
     end
   end
 
   describe "GET new" do
-    context "logged in as system admin" do
-      before :each do
-        sign_in sameer
-      end
+    let(:request) { get :new }
+    let(:assigns_assertion) { assigns[:employee].should be_a_new_record }
+    let(:page_behavior) { render_template 'new' }
+    it_should_behave_like "access by the admin", '@employee'
 
-      it "renders the new page" do
-        get :new
-        response.should render_template 'new'
-      end
-
-      it "assigns the employee with a new record" do
-        get :new
-        assigns[:employee].should be_a_new_record
-      end
-    end
-
-    context "logged in as a normal employee" do
-      before :each do
-        sign_in peter
-      end
-
-      it "can not access the new page" do
-        get :new
-        response.should redirect_to root_path
-      end
-    end
+    it_should_behave_like "denied by the normal employee", 'new'
   end
 
   describe "POST create" do
+    let(:request) { post :create, employee: attributes_for(:employee, email: 'test@originatechina.com', originate_start_date: Date.current) }
+    let(:assigns_assertion) { assigns[:employee].should be_persisted }
+    let(:page_behavior) { redirect_to employees_path }
+    it_should_behave_like 'access by the admin'
+
+    # refactor
+=begin
     context "logged in as system admin" do
       before :each do
         sign_in sameer
@@ -75,6 +63,7 @@ describe EmployeesController do
         response.should redirect_to employees_path
       end
     end
+=end
 
     context "logged in a normal employee" do
       before :each do
--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,3 +1,4 @@
+# metadata for shared_examples
 shared_examples "successful render page" do |page|
   it "renders the #{page} page"do
     response.should render_template page
@@ -10,16 +11,24 @@ shared_examples "assigns variable" do |variable|
   end
 end
 
-shared_examples "access by the admin" do |target_page, variable|
+shared_examples "page behaves" do
+  it "behaves" do
+    response.should page_behavior
+  end
+end
+
+# Invoked by outside
+shared_examples "access by the admin" do |variable|
   before :each do
     sign_in sameer
     request
   end
 
-  it_should_behave_like "successful render page", target_page
-  if variable
-    it_should_behave_like "assigns variable", variable
-  end
+  # it_should_behave_like "successful render page", target_page
+
+  it_should_behave_like "assigns variable", variable if variable
+
+  it_should_behave_like "page behaves"
 end
 
 shared_examples "access by the normal employee" do |target_page, variable|
@@ -29,7 +38,17 @@ shared_examples "access by the normal employee" do |target_page, variable|
   end
 
   it_should_behave_like "successful render page", target_page
-  if variable
-    it_should_behave_like "assigns variable", variable
+
+  it_should_behave_like "assigns variable", variable if variable
+end
+
+shared_examples "denied by the normal employee" do |page|
+  before :each do
+    sign_in peter
+    request
+  end
+
+  it "can not access the #{page} page" do
+    response.should redirect_to root_path
   end
 end
