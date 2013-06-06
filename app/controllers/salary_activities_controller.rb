96238ac1273b9d764962e2974efc20064544d520
peterzd.work <zdsunshine0640@126.com>
2013-06-06 14:15:50 +0800

    use server conserns for AJAX request

--- app/controllers/salary_activities_controller.rb
+++ app/controllers/salary_activities_controller.rb
@@ -81,8 +81,12 @@ class SalaryActivitiesController < ApplicationController
   def destroy
     @salary_activity = SalaryActivity.find(params[:id])
     @salary_activity.destroy
+    # flash[:success] = ' Destroyed the salary activity'
+    # redirect_to contract_salary_activities_path(@contract)
+    @salary_activities = @contract.salary_activities.order :effective_date
 
-    flash[:success] = ' Destroyed the salary activity'
-    redirect_to contract_salary_activities_path(@contract)
+    respond_to do |format|
+      format.js {}
+    end
   end
 end
