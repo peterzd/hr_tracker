dacd5513c603fba4ea624e821dff54789eec4b98
peterzd.work <zdsunshine0640@126.com>
2013-06-07 14:13:16 +0800

    make it work for ajax js

--- app/controllers/salary_activities_controller.rb
+++ app/controllers/salary_activities_controller.rb
@@ -20,6 +20,7 @@ class SalaryActivitiesController < ApplicationController
     @contract = Contract.where(id: params[:contract_id]).first
     @salary_activity = @contract.salary_activities.build()
     @discussion = Discussion.new
+
   end
 
   def new
