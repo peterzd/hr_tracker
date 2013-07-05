ebaff0dc9a01651fceeae1d68ef829d2943ed83d
peterzd.work <zdsunshine0640@126.com>
2013-07-05 14:42:14 +0800

    extract methods in bonus/table to view helper

--- app/helpers/bonuses_helper.rb
+++ app/helpers/bonuses_helper.rb
@@ -1,2 +1,6 @@
 module BonusesHelper
+  def can_operate?
+    operations = @operations.nil? ? true : @operations
+    ( can? :manage, Bonus ) && operations
+  end
 end
--- app/models/bonus.rb
+++ app/models/bonus.rb
@@ -1,4 +1,6 @@
 class Bonus < ActiveRecord::Base
   belongs_to :employee
   attr_accessible :amount, :comment, :distribution_date
+
+  default_scope order('distribution_date DESC')
 end
